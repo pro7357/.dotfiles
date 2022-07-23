0bc9fc454c24e1094d7e5ef30a64fa3b9dbfb331
pro7357 <pro7357@protonmail.com>
2022-07-24 07:47:19 +0800

    Update to startx on tty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
         exec startx -- -keeptty
+    else if test -z "$DISPLAY"; and string match -q "/dev/tty?" (tty)
+        exec startx -- -keeptty
     end
 end
